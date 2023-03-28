f7c65c9d8c0ad25439c94bad29fc24f56ce70caf
Nonlandz <88500373+Nonlandz@users.noreply.github.com>
2023-03-28 21:36:54 +0700

    update

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,85 +1,58 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<div id="app">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div class="login-page">
+   <transition name="fade">
+      <div v-if="!registerActive" class="wallpaper-login"></div>
+   </transition>
+   <div class="wallpaper-register"></div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+            <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
+               <h1>Sign In</h1>
+               <form class="form-group">
+                  <input v-model="emailLogin" type="email" class="form-control" placeholder="Email" required>
+                  <input v-model="passwordLogin" type="password" class="form-control" placeholder="Password" required>
+                  <input type="submit" class="btn btn-primary" @click="doLogin">
+                  <p>Don't have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign up here</a>
+                  </p>
+                  <p><a href="#">Forgot your password?</a></p>
+               </form>
+            </div>
 
-  <RouterView />
+            <div v-else class="card register" v-bind:class="{ error: emptyFields }">
+               <h1>Sign Up</h1>
+               <form class="form-group">
+                  <input v-model="emailReg" type="email" class="form-control" placeholder="Email" required>
+                  <input v-model="passwordReg" type="password" class="form-control" placeholder="Password" required>
+                  <input v-model="confirmReg" type="password" class="form-control" placeholder="Confirm Password" required>
+                  <input type="submit" class="btn btn-primary" @click="doRegister">
+                  <p>Already have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign in here</a>
+                  </p>
+               </form>
+            </div>
+         </div>
+      </div>
+
+   </div>
+</div>
+
+</div>
 </template>
 
+
+<script>
+export default {
+
+  
+}
+
+
+</script>
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
