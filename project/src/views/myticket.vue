79a9b4607cb64910fa1bd2a29f0f7096f88f94fb
Choatchda Aupayokin <88426286+Choatchda@users.noreply.github.com>
2023-04-03 23:16:52 +0700

    Update myticket.vue

--- project/src/views/myticket.vue
+++ project/src/views/myticket.vue
@@ -5,5 +5,35 @@ import Nav from '../components/Nav.vue'
    <main>
     <Nav />
   </main>
-  This is My ticket
+  <div class="container" >
+  <div class="card" style="width: 100%; margin-top: 5%;">
+  <div class="card-body">
+    <h5 class="card-title text-center">ตั๋วของฉัน</h5>
+
+    <div class="container" style="height: 100%; display: flex; justify-content: center; align-items: center; margin-top: 3%;">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://u7.uidownload.com/vector/301/854/vector-cartoon-steam-train-vector-eps.jpg" class="img-fluid rounded-start" style="height: 100%;">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">วัน:      เวลา:</h5>
+        <p class="card-text">ต้นทาง-ปลายทาง</p>
+        <p class="card-text">จำนวน</p>
+        <p class="card-text">ราคา:</p>
+        <p class="card-text">ประเภทตั๋ว:</p>
+        <p class="card-text"><small class="text-body-secondary">สวัสดีครับสุดหล่อ</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+  </div>
+    
+ 
+  </div>
+</div>
 </template>
+ 
