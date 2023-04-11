048308a1b371d857f6c6b5f7d491b8ebecd49288
Nonlandz <nonlandza123@gmail.com>
2023-04-11 20:37:34 +0700

    update

--- project/src/views/register.vue
+++ project/src/views/register.vue
@@ -1,69 +1,64 @@
-<script setup>
-import Nav from '../components/Nav.vue'
-</script>
 <template>
-    <Nav></Nav>
-    <div class="container-fluid" style="padding-top:30px; ">
-        <div class="d-flex justify-content-center " style="margin:auto">
-            <form class="row g-3" style="width:40%">
-                <h2 class="text-center">สมัครสมาชิก</h2>
+  <div>
+    <b-container class="mt-5">
+      <b-row class="justify-content-md-center">
+        <b-col md="6">
+          <b-card class="shadow">
+            <b-card-title class="text-center mb-4">Register</b-card-title>
+            <b-form @submit.prevent="register">
+              <b-form-group label="Username" label-for="username">
+                <b-form-input id="username" v-model="user.username" type="text" required />
+              </b-form-group>
 
-                <!-- fitsname -->
-                <div class="col-md-6">
-                  <input type="text" class="form-control" placeholder="ชื่อจริง" id="fname_re" name="fname_re">
-                </div>
-                <!-- lastname -->
-                <div class="col-md-6">
-                  <input type="text" class="form-control" id="lname-re" name="lname-re" placeholder="นามสกุล">
-                </div>
-                <!-- username -->
-                <div class="col-12">
-                    <div class="input-group">
-                        <div class="input-group-text"><i class="fa-solid fa-user"></i></div>
-                        <input type="text" class="form-control" id="username_re" placeholder="ชื่อผู้ใช้">
-                    </div>
-                  <!-- email -->
-                </div>
-                <div class="col-12">
-                  <div class="input-group">
-                    <div class="input-group-text"><i class="fa-solid fa-envelope"></i></div>
-                    <input type="text" class="form-control" id="email_re" placeholder="ที่อยู่อีเมล">
-                  </div>
-                </div>
-                <!-- password -->
-                <div class="col-12">
-                    <div class="input-group">
-                      <div class="input-group-text"><i class="fa-solid fa-key"></i></div>
-                      <input type="password" class="form-control" id="password_re" placeholder="รหัสผ่าน">
-                    </div>
-                  </div>
-                  <div class="col-12">
-                    <div class="input-group">
-                      <div class="input-group-text"><i class="fa-solid fa-key"></i></div>
-                      <input type="password" class="form-control" id="confirmpassword_re" placeholder="ยืนยันรหัสผ่าน">
-                    </div>
-                  </div>
-              
-                <!-- checkbox  -->
-                <div class="col-12">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                      ฉันยอมรับว่า Rex Orange County คือศิลปินที่ดีที่สุดในโลกใบนี้
-                    </label>
-                  </div>
-                </div>
-                <!-- submit button -->
-                <div class="col-12 text-center">
-                  <button type="submit" class="btn btn-primary" style="width:100%">สมัครสมาชิก</button>
-                </div>
-              </form>
-        </div>
-    </div>
+              <b-form-group label="Password" label-for="password">
+                <b-form-input id="password" v-model="user.password" type="password" required />
+              </b-form-group>
+
+              <b-form-group label="First Name" label-for="firstName">
+                <b-form-input id="firstName" v-model="user.firstName" type="text" required />
+              </b-form-group>
+
+              <b-form-group label="Last Name" label-for="lastName">
+                <b-form-input id="lastName" v-model="user.lastName" type="text" required />
+              </b-form-group>
+
+              <b-form-group label="Telephone Number" label-for="telNumber">
+                <b-form-input id="telNumber" v-model="user.telNumber" type="tel" required />
+              </b-form-group>
+
+              <b-button type="submit" variant="primary" class="w-100 mt-3">Register</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
-<style scoped>
-    body{
-        background-color: black;  
-        justify-content:center;
-    }
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        telNumber: '',
+      },
+    };
+  },
+  methods: {
+    register() {
+      console.log(this.user);
+      // Perform registration logic here
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  border-radius: 1rem;
+}
 </style>
