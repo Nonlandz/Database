583594fff7fd0c6072699bdd07f5330080d1c0bd
Nonlandz <nonlandza123@gmail.com>
2023-04-14 17:14:57 +0700

    Update Nav.vue

--- project/src/components/Nav.vue
+++ project/src/components/Nav.vue
@@ -13,7 +13,7 @@
           <a href="/home" class="text-gray-800 text-xl font-semibold hover:text-purple-600 mr-4 no-underline bg-white">หน้าแรก</a>
           <a href="/myticket" class="text-gray-800 text-xl font-semibold hover:text-purple-600 mr-4 no-underline bg-white">ตั๋วของฉัน</a>
           <a href="#" class="text-gray-800 text-xl font-semibold hover:text-purple-600 mr-4 no-underline bg-white">กระเป๋า</a>
-          <a href="#" class="text-gray-800 text-xl font-semibold hover:text-purple-600 no-underline bg-white">แลกของรางวัล</a>
+          <a href="/prize" class="text-gray-800 text-xl font-semibold hover:text-purple-600 no-underline bg-white">แลกของรางวัล</a>
 
           <!-- <a href="#" class="text-gray-800 text-xl font-semibold hover:text-purple-600 no-underline bg-white">{{ user.Username}}</a> -->
         </div>
