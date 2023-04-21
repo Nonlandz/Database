e4ea275bd96eec71d8f1ed853e88cbf6b9c9e183
Nonlandz <nonlandza123@gmail.com>
2023-04-21 17:08:11 +0700

    update

--- project/backend/config.js
+++ project/backend/config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '12347856',
-  database: 'Database',
+  password: 'Impact12',
+  database: 'DBproject',
 
 
   waitForConnections: true,
